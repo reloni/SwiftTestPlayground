a8c2c02e8aaf0a646869f956970c85d50d3a77aa
Anton Efimenko <reloni@ya.ru>
2018-08-16 21:15:30 +0300

    Fixed sieve of Sundaram

--- Test.playground/Pages/Shit.xcplaygroundpage/Contents.swift
+++ Test.playground/Pages/Shit.xcplaygroundpage/Contents.swift
@@ -2,7 +2,9 @@
 
 import Foundation
 
-//sieveOfSundaram(n: 100)
+sieveOfSundaram(n: 100)
+
+
 //easySieve(n: 10)
 //sieveOfSundaram(n: 10)
 
--- Test.playground/Sources/Sieves.swift
+++ Test.playground/Sources/Sieves.swift
@@ -1,16 +1,17 @@
 import Foundation
 
 public func sieveOfSundaram(n: Int) -> [Int] {
-    var results = Array.init(repeating: false, count: n)
-    for i in 1..<n {
+    let numberOfPrimes = (n - 2) / 2
+    var results = Array.init(repeating: false, count: numberOfPrimes + 1)
+    for i in 1...numberOfPrimes {
         var j = i
-        while i + j + 2 * i * j < n {
+        while i + j + 2 * i * j <= numberOfPrimes {
             results[i + j + 2 * i * j] = true
             j += 1
         }
     }
     
-    return [2] + (1..<n).compactMap { index -> Int? in
+    return [2] + (1...numberOfPrimes).compactMap { index -> Int? in
         if !results[index] {
             return 2 * index + 1
         } else {
