4726b75aba406dd80d24885b8ff7eb7cf5459e96
Anton Efimenko <reloni@ya.ru>
2018-10-06 18:13:27 +0300

    Add capitalizeString

---
+++ Test.playground/Sources/CapitalizeString.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+/*
+ Write a program that will correct an input string to use proper capitalization and spacing. Allowed punctuations are the period ( . ), question mark ( ? ), and exclamation ( ! ). Make sure that single space always follows commas ( , ), colons ( : ), semicolons ( ; ) and all other punctuation. The input string will be a valid English sentence.
+ 
+ capitalizeString(«first, solve the problem.then, write the code.») // «First, solve the problem. Then, write the code.»
+ 
+ capitalizeString(«this is a test… and another test.») // «This is a test… And another test.»
+ */
+
+public func capitalizeString(_ value: String) -> String {
+    return ""
+}
